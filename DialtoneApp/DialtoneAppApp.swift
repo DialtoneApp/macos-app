c9f996228ac1ffb160b05916b0003624f66f64d4
aa <oneone@gmail.com>
2026-04-22 12:36:42 -0700

    new app

---
+++ DialtoneApp/DialtoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialtoneAppApp.swift
+//  DialtoneApp
+//
+//  Created by aa on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct DialtoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
